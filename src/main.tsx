a57c0d19db3bf0485c6682e1f8b1e2026efbc6a6
csorrentino <csorrentino@sysmanagement.it>
2023-05-26 09:50:55 +0200

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+// import ReactDOM from 'react-dom/client'
+
+import App from './App.tsx'
+import './index.css'
+import  ReactDOM  from 'react-dom'
+const rootNode = document.getElementById('root');
+ReactDOM.render(<App />, rootNode);
+
+
